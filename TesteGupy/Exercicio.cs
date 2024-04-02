f2ea3b327e3ea555bedb4b64714154773ab654d4
Bartcelo <bart.celo@hotmail.com>
2024-04-02 13:35:29 -0300

    fisrt commit

---
+++ TesteGupy/Exercicio.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.Metadata;
+using ImTools;
+
+namespace TesteGupy
+{
+    internal class Exercicio
+    {
+        public void primeiroExercicio()
+        {
+
+            int indice = 13;
+            int k = 0;
+            int soma = 0;
+
+            for (k = 0; k < indice; k++)
+            {
+                soma = soma + k;
+            }
+
+           Console.WriteLine(soma);
+
+        }
+
+        public void segundoExercicio()
+        {
+
+            Console.Write("Digite um número para verificação de Fibonacci: ");
+            int verificanumber = int.Parse(Console.ReadLine());
+
+            static bool fibo(int n)
+            {
+                int a = 0;
+                int b = 1;
+
+                while (b < n)
+                {
+                    int temp = a;
+                    a = b;
+                    b = temp + b;
+                }
+
+                return b == n;
+            }
+
+            if (fibo(verificanumber))
+            {
+                Console.WriteLine( $"O número {verificanumber} está na sequência de Fibonacci.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {verificanumber} não está na sequência de Fibonacci.");
+            }
+        }
+        public void terceiroExercicioA()
+        {
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                int impar = i;
+
+                if (impar % 2 != 0)
+                {
+                    list.Add(impar);
+                    Console.WriteLine(impar);
+                }
+            }
+        }
+
+        public void terceiroExercicioB()
+        {
+            List<int> list = new List<int>();
+
+            int i = 2;
+            int total = 256;
+
+
+            while (i < total)
+            {
+                i = i * 2;
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+
+       public void terceiroExercicioC()
+        {
+            List<int> list = new List<int>();
+            int i;
+            for (i = 0; i < 65; i++)
+            {
+                double value = Math.Pow(i, 2);
+
+                if (value < 65)
+                {
+                    Console.WriteLine(value);
+
+                }
+
+            }
+        }
+
+        public void terceiroExercicioD()
+        {
+            List<int> list = new List<int>();
+
+            int i = 0;
+            int total = 13;
+
+
+            while (i < total)
+            {
+                int j = i;
+
+
+
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(Math.Pow(i, 2));
+
+                }
+                i++;
+            }
+        }
+
+        public void terceiroExercicioE()
+        {
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i <10; i++)
+            {
+            Console.WriteLine(b);
+
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+        }
+        public void terceiroExercicioF() {
+
+            Console.WriteLine("f) 2,10, 12, 16, 17, 18, 19, __200__");
+        
+        }
+
+        public void quartoExercicio() {
+            Console.WriteLine("Primeiro apertava no interruptor A e deixo ele ligado por um periodo,desligo o interruptor A e ligo o interruptor B e vou imediatamente para a sala onde tem as lampadas, o interruptor A estara com a lampada quente porem apagada o Interruptor B esta com a Lampada acesa e o interruptor C estara com a Lampada apagada e fria. ");
+        
+        }
+
+        public void quintoExercicio()
+        {
+
+            Console.WriteLine("Digite uma palavra com mais de dois caracteres ou uma frase para ser invertida");
+
+            String palavra = Console.ReadLine();
+
+            if (palavra == null || palavra == "")
+            {
+                Console.WriteLine("A palavra deve conter pelo menos dois caracteres.");
+            }
+
+            if (palavra.Length > 1)
+            {
+
+                String invertida = "";
+                for (int i = palavra.Length; i > 0; i--)
+                {
+
+                    invertida += palavra[i - 1];
+
+                }
+                Console.WriteLine(invertida);
+            }
+        }
+    }
+}
+
+
+
+    
+
+
+
---
+++ TesteGupy/Program.cs
@@ -0,0 +1,86 @@
+﻿using Baseline;
+using ImTools;
+using System.Reflection.Metadata;
+
+namespace TesteGupy
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Exercicio exercicio = new Exercicio();
+
+            int saida = 0;
+            while (saida == 0) {
+
+
+                Console.WriteLine("Digite qual exercicio você deseja analisar sabendo que:" +
+                                  " \n 1 representa o primeiro" +
+                                  "\n 2 represnta o segundo " +
+                                  "\n 3a representa a parte a do terceiro" +
+                                  " \n 3a representa a parte b do terceiro " +
+                                  "\n 3c representa a parte c do terceiro  " +
+                                  "\n 3c representa a parte c do terceiro  " +
+                                  "\n 3e representa a parte e do terceiro " +
+                                  "\n 3f representa a parte f do terceiro " +
+                                  "\n 4 representa o quarto" +
+                                  "\n 5 representa o quinto" +
+                                  " \n 6 saida  ");
+
+
+                String resposta = Console.ReadLine();
+
+                if (resposta == "1")
+                {
+                    exercicio.primeiroExercicio();
+                }
+                else if (resposta == "2")
+                {
+                    exercicio.segundoExercicio();
+                }
+                else if (resposta == "3a")
+                {
+                    exercicio.terceiroExercicioA();
+                }
+                else if (resposta == "3b")
+                {
+                    exercicio.terceiroExercicioB();
+                }
+                else if (resposta == "3c")
+                {
+                    exercicio.terceiroExercicioC();
+                }
+                else if (resposta == "3d")
+                {
+                    exercicio.terceiroExercicioD();
+                }
+                else if (resposta == "3e")
+                {
+                    exercicio.terceiroExercicioE();
+                }
+                else if (resposta == "3f")
+                {
+                    exercicio.terceiroExercicioF();
+                }
+                else if (resposta == "4")
+                {
+                    exercicio.quartoExercicio();
+                }
+                else if (resposta == "5")
+                {
+                    exercicio.quintoExercicio();
+                }
+                else if (resposta == "6")
+                {
+                    saida = 8;
+                    Console.WriteLine("TCHAUUUUUUUUUU");
+                }
+                else
+                {
+                    Console.WriteLine("O valor digitado esta incorreto");
+                }
+            };
+        }
+    }
+}
